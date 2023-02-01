d05e4d72d848a61ae232b224cc7698b76939a45b
Sammy Henningsson <sammyhenningsson@hey.com>
2023-02-01 22:24:36 +0000

    try ci

--- test/safe_by_default_test.rb
+++ test/safe_by_default_test.rb
@@ -22,8 +22,7 @@ class SafeByDefaultTest < Minitest::Test
   end
 
   def test_add_index_corruption
-    # TODO fix
-    skip # unless postgresql?
+    skip unless postgresql?
     outside_developer_env do
       with_target_version(14.3) do
         assert_unsafe AddIndex, "can cause silent data corruption in Postgres 14.0 to 14.3"
